556c62ee4f60ca1f10edc3e4f53cac6eaf7305fa
Thanasis Karetsos <thanasis@karetsos.me>
2025-08-05 22:35:04 +0300

    Add config & plugins

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        enabled = false,
+      },
+      terminal = {
+        win = {
+          position = "float",
+          relative = "editor",
+        },
+      },
+      indent = {
+        enabled = false,
+      },
+    },
+  },
+}
