8bfdd9ada0d96ba1d88067f60cac6c567aa0923f
Thanasis Karetsos <thanasis@karetsos.me>
2026-08-20 17:37:28 +0300

    add golang lsp config

---
+++ lua/plugins/go.lua
@@ -0,0 +1,22 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            hints = {
+              assignVariableTypes = false,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = false,
+              rangeVariableTypes = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
